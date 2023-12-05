aa043a1e5e2f75629854c839d6c5c2ffd10fea36
Edgar Sakwa <sakwaedgar57@gmail.com>
2023-12-05 14:50:49 +0300

    added requirements.txt

--- Insurance/insurancemanagement/admin.py
+++ Insurance/insurancemanagement/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from insurancemanagement.models import Category, Policy, PolicyRecord, Question
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Policy)
+admin.site.register(PolicyRecord)
+admin.site.register(Question)
